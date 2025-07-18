4f8a7edeaabc1ac392c9af4341f7d6bc590a6990
acoent <tytan567@gmail.com>
2025-07-17 22:23:28 -0500

    Fix checkstyle: adjust indentation

--- src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeStorage.java
@@ -6,9 +6,13 @@ import java.util.List;
 
 public interface LikeStorage {
     void addLike(long filmId, long userId);
+
     void removeLike(long filmId, long userId);
+
     int getLikesCount(long filmId);
+
     List<Film> getTopLikedFilms(int limit);
+
     void removeAllLikesByFilmId(long filmId);
 
 }
